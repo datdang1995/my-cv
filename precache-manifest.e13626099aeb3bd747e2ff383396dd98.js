1d36ce99d89b4cbe9c55436b013de1f76bed9c74
Dat Dang <dangtuandat1995@gmail.com>
2020-02-11 21:02:34 +0700

    Updates

---
+++ precache-manifest.e13626099aeb3bd747e2ff383396dd98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341f130b11be38059ff24b224eaf60e6",
+    "url": "/my-cv/index.html"
+  },
+  {
+    "revision": "4dcca91b82dc37e37bcc",
+    "url": "/my-cv/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e94c7b8268a345fba51e",
+    "url": "/my-cv/static/js/2.56e7d182.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-cv/static/js/2.56e7d182.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dcca91b82dc37e37bcc",
+    "url": "/my-cv/static/js/main.e80d4e7c.chunk.js"
+  },
+  {
+    "revision": "3072575974a33a862144",
+    "url": "/my-cv/static/js/runtime-main.602423d2.js"
+  }
+]);
